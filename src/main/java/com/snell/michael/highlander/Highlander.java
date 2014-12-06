952d62d6459263dabcde860efabb9a49dc85ee19
Michael Snell <michael@snell.com>
2014-12-06 15:04:49 +0000

    Second pass

--- src/main/java/com/snell/michael/highlander/Highlander.java
+++ src/main/java/com/snell/michael/highlander/Highlander.java
@@ -1,22 +1,68 @@
 package com.snell.michael.highlander;
 
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
 
+@SuppressWarnings("WeakerAccess")
 public class Highlander {
-    // TODO Optimisation for lists etc
+    private static final String NO_ELEMENTS_FOUND = "Expected onle element but found no elements ";
+    private static final String EXPECTED_ONE_ELEMENT_BUT_FOUND = "Expected one element but found ";
+
+    private Highlander() {}
+
+    @SafeVarargs
+    public static <T> T only(T... tees) {
+        if (tees.length == 0) {
+            throw new RuntimeException(NO_ELEMENTS_FOUND);
+        } else if (tees.length == 1) {
+            return tees[0];
+        } else {
+            throw new RuntimeException(EXPECTED_ONE_ELEMENT_BUT_FOUND + tees.length);
+        }
+    }
+
+    public static <T> T only(List<T> list) {
+        if (list.isEmpty()) {
+            throw new RuntimeException(NO_ELEMENTS_FOUND);
+        } else if (list.size() == 1) {
+            return list.get(0);
+        } else {
+            throw new RuntimeException(EXPECTED_ONE_ELEMENT_BUT_FOUND + list.size());
+        }
+    }
+
+    public static <T> T only(Collection<T> collection) {
+        if (collection.isEmpty()) {
+            throw new RuntimeException(NO_ELEMENTS_FOUND);
+        } else if (collection.size() == 1) {
+            return collection.iterator().next();
+        } else {
+            throw new RuntimeException(EXPECTED_ONE_ELEMENT_BUT_FOUND + collection.size());
+        }
+    }
+
     public static <T> T only(Iterable<T> iterable) {
-        Iterator<T> iterator = iterable.iterator();
+        return only(iterable.iterator());
+    }
+
+    public static <T> T only(Stream<T> stream) {
+        return only(stream.iterator());
+    }
+
+    private static <T> T only(Iterator<T> iterator) {
         if (iterator.hasNext()) {
             T t = iterator.next();
             if (iterator.hasNext()) {
-                throw new RuntimeException("Expected one element but found two or more");
+                throw new RuntimeException(EXPECTED_ONE_ELEMENT_BUT_FOUND + "two or more");
             } else {
                 return t;
             }
         } else {
-            throw new RuntimeException("No elements found");
+            throw new RuntimeException(NO_ELEMENTS_FOUND);
         }
     }
 
-    // TODO Support arrays, streams etc
+    // TODO Collector?
 }
--- src/test/java/com/snell/michael/highlander/HighlanderTest.java
+++ src/test/java/com/snell/michael/highlander/HighlanderTest.java
@@ -2,16 +2,81 @@ package com.snell.michael.highlander;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.function.Predicate;
 
+import static com.snell.michael.highlander.Highlander.only;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
 public class HighlanderTest {
-    public static final Collection<String> COLLECTION = Arrays.asList("ONE");
+    // Arrays
 
     @Test
-    public void onlyIterable() {
-        assertEquals("ONE", Highlander.only(COLLECTION));
+    public void arrayOne() {
+        assertEquals("ONE", only("ONE"));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void arrayTwo() {
+        only("ONE", "TWO");
+    }
+
+    // Lists
+
+    @Test
+    public void listOne() {
+        assertEquals("ONE", only(asList("ONE")));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void listTwo() {
+        only(asList("ONE", "TWO"));
+    }
+
+    // Collections
+
+    @Test
+    public void collectionOne() {
+        assertEquals("ONE", only((Collection<String>) asList("ONE")));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void collectionTwo() {
+        only((Collection<String>) asList("ONE", "TWO"));
+    }
+
+    // Iterables
+
+    @Test
+    public void iterableOne() {
+        assertEquals("ONE", only((Iterable<String>) asList("ONE")));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void iterableTwo() {
+        only((Iterable<String>) asList("ONE", "TWO"));
+    }
+
+    // Streams
+
+    @Test
+    public void streamOne() {
+        assertEquals("ONE", only(asList("ONE").stream()));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void streamTwo() {
+        only(asList("ONE", "TWO").stream());
+    }
+
+    @Test
+    public void streamFilter() {
+        assertEquals("ONE", only(asList("ONE", "TWO").stream().filter(new Predicate<String>() {
+            @Override
+            public boolean test(String t) {
+                return "ONE".equals(t);
+            }
+        })));
     }
 }
